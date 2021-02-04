a432e10d695e05dd80cbf6396942907cb3dc67e7
sirima2547 <69945455+sirima2547@users.noreply.github.com>
2021-02-04 12:30:48 +0700

    Add files via upload

---
+++ condition201.js
@@ -0,0 +1,6 @@
+const n = 12;
+const m = 3;
+for(let i = 1; i<=n; i++)
+{
+    console.log( m + ' x ' + i + ' = ' + i*m);
+}
---
+++ condition202.js
@@ -0,0 +1,9 @@
+let i = 1;
+let m = 2;
+while(i<12)
+{
+   //console.log('I Love Earth');
+   //console.log('1 x' + i + '=' +i*1);
+   console.log(m + ' x ' +  i + ' = ' + i*m);
+   i++; //i = i+1
+}
